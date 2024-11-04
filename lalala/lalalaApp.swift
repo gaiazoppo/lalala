7a40c32e8e255d25327155517fa3421c5678cc9e
Gaia Zoppo <gaiazoppo27@gmail.com>
2024-11-04 15:18:53 +0100

    Initial Commit

---
+++ lalala/lalalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lalalaApp.swift
+//  lalala
+//
+//  Created by Gaia zoppo on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct lalalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
